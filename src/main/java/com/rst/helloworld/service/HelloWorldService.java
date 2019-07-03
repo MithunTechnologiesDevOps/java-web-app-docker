bf590a0a131c8d263efb20078d4e7cef3025005d
balajireddylachhannagari <devopstrainingbanglore@gmail.com>
2019-07-03 22:52:40 +0530

    First Commit

---
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -0,0 +1,33 @@
+package com.rst.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+	public String getDesc() {
+
+		logger.debug("getDesc() is executed!");
+
+		return "Maven + Spring MVC + Jenkins + Docker Hello World Example";
+
+	}
+
+	public String getTitle(String name) {
+
+		logger.debug("getTitle() is executed! $name : {}", name);
+
+		if(StringUtils.isEmpty(name)){
+			return "Hello World";
+		}else{
+			return "Hello " + name;
+		}
+		
+	}
+
+}
