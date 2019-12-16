c7bbe082ff29046b9ebbf3bc5fc1957a0de1dcc7
devopstrainingbanglore <43769749+devopstrainingbanglore@users.noreply.github.com>
2019-12-16 21:49:59 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -14,7 +14,7 @@ public class HelloWorldService {
 
 		logger.debug("getDesc() is executed!");
 
-		return "Maven + Spring MVC + Jenkins + Docker Hello World Example";
+		return "Maven + Spring MVC + Jenkins + Docker Example";
 
 	}
 
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello World";
+			return "Hello Docker";
 		}else{
 			return "Hello " + name;
 		}
